c7dfca3898621ea2e78abfa24154fd9f851f2492
nimacalhor <nimakalhor25@gmail.com>
2022-02-05 10:48:10 +0330

    add_ map area for two cities

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,2 @@
 export const BASE_URL = "http://localhost:3000/"
+export const TIMEOUT_SEC = 10;
--- public/js/helper.js
+++ public/js/helper.js
@@ -1,10 +1,17 @@
-export const getJSON = async function(url){
+import { TIMEOUT_SEC } from "./config.js"
+
+const timeOut = async function (sec) {
+    return new Promise((_, reject) => 
+        setTimeout(() => reject(new Error("request took a long time")), sec * 1000)
+    )
+}
+
+export const getJSON = async function (url) {
     try {
-        const res = await fetch(url);
-        if(!res.ok) throw new Error(res.status)
+        const res = await Promise.race([fetch(url), timeOut(TIMEOUT_SEC)]);
+        if (!res.ok) throw new Error(res.status)
         return await res.json();
     } catch (err) {
-        console.log(err)
         throw err;
     }
 }
